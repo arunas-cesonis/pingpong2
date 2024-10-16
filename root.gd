cb22a6a965b33d665a211f5d0f9a24686ea17a09
Arunas <karunce@gmail.com>
2024-10-16 21:42:02 +0100

    Calculate collision reflection vector

--- root.gd
+++ root.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var ball_direction = Vector2(-1.0, 1.0).normalized()
 var ball_speed = 500.0
-var p1: CharacterBody2D = null
+@onready var p1: CharacterBody2D = $Paddle1
 var ball: CharacterBody2D = null
 var player_speed = 800.0
 var walls = []
@@ -13,7 +13,6 @@ var walls = []
 func _ready() -> void:
 	walls.push_back(self.find_child("WallLeft"))
 	walls.push_back(self.find_child("WallRight"))
-	p1 = self.find_child("Paddle1")
 	ball = self.find_child("Ball")
 	ball.velocity = ball_speed * ball_direction
 
@@ -23,14 +22,18 @@ func _input(event) -> void:
 func _physics_process(delta: float) -> void:
 	p1.move_and_collide(p1.velocity * delta)
 	var col = ball.move_and_collide(ball.velocity * delta)
-	print(Vector2.RIGHT)
 	if col != null:
 		if -1 != walls.find(col.get_collider()):
 			if col.get_normal() == Vector2.RIGHT:
 				ball_direction.x = -ball_direction.x
 				ball_direction = ball_direction.normalized()
-	ball.velocity = ball_direction * ball_speed			
-	
+		elif p1 == col.get_collider():
+			var d = ball_direction
+			var n = col.get_normal()
+			var r = d - 2.0 * (d.dot(n)) * n
+			ball_direction = r.normalized()
+			
+	ball.velocity = ball_direction * ball_speed
 			# print(ball_direction.angle(), col.get_normal())
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
