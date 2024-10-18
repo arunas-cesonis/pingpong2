24fe89dd5e727b3197931313b11ff11e2994d215
Arunas <karunce@gmail.com>
2024-10-18 12:06:45 +0100

    Add debug drawing & reseting scene

--- root.gd
+++ root.gd
@@ -2,27 +2,40 @@ extends Node2D
 
 @onready var player: AnimatableBody2D = $Player
 @onready var ball: AnimatableBody2D = $Ball
+@onready var debug: Node2D = $Debug
 var ball_velocity = Vector2.from_angle(PI / 1.5) * 500.0
 var player_velocity = Vector2.ZERO
 const player_speed = 500.0
-var walls = []
 var halt = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	walls.push_back($WallLeft)
-	walls.push_back($WallRight)
-	get_window().position -= get_window().position / 2
-	get_window().size *= 2.0
+	pass
+	#get_window().position -= get_window().position / 2
+	#get_window().size *= 2.0
 
 func _input(event) -> void:
-	pass
+	if Input.is_action_just_pressed("Reset"):
+		get_tree().reload_current_scene()
 
 func _physics_process(delta: float) -> void:
 	player.move_and_collide(player_velocity * delta)
 	var collision = ball.move_and_collide(ball_velocity * delta)
 	if collision:
-		ball_velocity = ball_velocity.bounce(collision.get_normal())
+		var normal: Vector2 = collision.get_normal()
+		if collision.get_collider() == player:
+			var offset = player.position.x - collision.get_position().x
+			var shape: RectangleShape2D = player.get_child(0).shape
+			var offset_normalized = offset / shape.size.x
+			var normal_offseted = Vector2.from_angle(normal.angle() - offset_normalized * 0.5)
+			normal = normal_offseted
+			
+		$Debug.bounce.velocity_in = ball_velocity
+		ball_velocity = ball_velocity.bounce(normal)
+		$Debug.bounce.velocity_out = ball_velocity
+		$Debug.bounce.position = collision.get_position()
+		$Debug.bounce.normal = normal
+		$Debug.queue_redraw()
 		
 func _process(delta: float) -> void:
 	var direction = 0.0
