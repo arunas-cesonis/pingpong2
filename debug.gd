4ef9da1961e9ea2725308f4ed65975de71ee842f
Arunas Cesonis <karunce@gmail.com>
2024-11-02 15:00:27 +0000

    Bounce angle limiting

--- debug.gd
+++ debug.gd
@@ -5,10 +5,11 @@ var bounce = {
 	position = Vector2(0.0, 0.0),
 	velocity_in = Vector2(0.0, 0.0),
 	velocity_out = Vector2(0.0, 0.0),
-	normal = Vector2(0.0, 0.0)
+	velocity_out_unlimited = Vector2(0.0, 0.0),
+	normal = Vector2(0.0, 0.0),
 }
 var values = {}
-const size = 100.0
+const BOUNCE_SIZE := 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,19 +24,40 @@ func update_value(key: String, value: Variant) -> void:
 		queue_redraw()
 	values[key] = value
 
-func _draw():
-	# reflection
-	var p = bounce.position
-	var vin = bounce.velocity_in.normalized() * size
-	var vout = bounce.velocity_out.normalized() * size
-	var n = bounce.normal.normalized() * size
+func _draw_text(s: String, pos: Vector2) -> void:
+	var default_font = ThemeDB.fallback_font
+	var default_font_size = ThemeDB.fallback_font_size
+	var size = default_font.get_string_size(s, HORIZONTAL_ALIGNMENT_LEFT, -1, default_font_size)
+	# draw_rect(Rect2(pos - Vector2(0.0, size.y), size), Color(1.0, 1.0, 1.0, 0.5), true, 0.0)
+	var vp := get_viewport_rect().size
+	var pos_l = Vector2(minf(pos.x, vp.x - size.x), minf(pos.y, vp.y - size.y))
+	draw_string(default_font, pos_l, s, HORIZONTAL_ALIGNMENT_LEFT, -1, default_font_size)
+
+func _draw_bounce(b: Dictionary) -> void:
+	if b.velocity_in.is_zero_approx():
+		return
+	var p = b.position
+	var vin = b.velocity_in.normalized() * BOUNCE_SIZE
+	var vout = b.velocity_out.normalized() * BOUNCE_SIZE
+	var voutu = b.velocity_out_unlimited.normalized() * BOUNCE_SIZE
+	var n = b.normal.normalized() * BOUNCE_SIZE
+	var angle = b.normal.angle_to(b.velocity_out)
+	var angle_u = b.normal.angle_to(b.velocity_out_unlimited)
+
 	draw_line(p - vin, p, Color.WHITE, 2.0)
 	draw_line(p, p + n, Color.RED, 2.0)
 	draw_line(p, p + vout, Color.WHITE, 2.0)
+	draw_dashed_line(p, p + voutu, Color.WHITE, 2.0, 4.0)
+	_draw_text("%.2f -> %.2f" % [rad_to_deg(angle_u), rad_to_deg(angle)], p + vout)
+
+
+func _draw():
+	# bounce
+	_draw_bounce(bounce)
 	# values
-	var default_font = ThemeDB.fallback_font
-	var default_font_size = ThemeDB.fallback_font_size
-	var h = 32.0
+	var h := 32.0
 	for k in values:
-		draw_string(default_font, Vector2(32.0, h), k + ": " + str(values[k]), HORIZONTAL_ALIGNMENT_LEFT, -1, default_font_size)
+		var s = k + ": " + str(values[k])
+		var pos := Vector2(32.0, h)
+		_draw_text(s, pos)
 		h = h + 24.0
--- game.gd
+++ game.gd
@@ -13,17 +13,29 @@ const RotatingBrick = preload("res://rotating_brick.gd")
 @onready var bricks: Node2D = $Bricks
 @onready var debug: Node2D = $Debug
 
+@export_group("Common")
 @export var initial_ball_direction := Vector2(0.0, 1.0)
 @export var initial_ball_speed := 500.0
 @export var player_speed := 500.0
 @export var reflect_amount := 0.5
 
+@export_group("Ball collision")
+@export_category("Wall")
+@export var wall_angle_limit := 180.0
+@export var wall_acceleration := 0.0
+@export var wall_spawn_node: PackedScene = null
+@export_category("Player")
+@export var player_angle_limit := 180.0
+@export var player_acceleration := 0.0
+@export var player_spawn_node: PackedScene = null
+@export_category("Brick")
+@export var brick_angle_limit := 180.0
+@export var brick_acceleration := 0.0
+@export var brick_spawn_node: PackedScene = null
+
 @onready var player_velocity = Vector2.ZERO
 
 var score := 0
-
-# To be used to add acceleration to ball
-@onready var player_velocity_avg = Vector2.ZERO
 
 @onready var ball_speed = initial_ball_speed
 @onready var ball_direction = initial_ball_direction
@@ -41,31 +53,46 @@ func _input(_event) -> void:
 	if Input.is_action_just_pressed("Reset"):
 		get_tree().reload_current_scene()
 
+func _ball_velocity() -> Vector2:
+	return ball_direction * ball_speed
+
+func _limit_bounce_angle(normal: Vector2, direction: Vector2, limit: float) -> Vector2:
+	var angle = normal.angle_to(direction)
+	var new_angle = signf(angle) * minf(abs(angle), deg_to_rad(limit))
+	return Vector2.from_angle(normal.angle() + new_angle)
+
 func _physics_process(delta: float) -> void:
 	player.move_and_collide(player_velocity * delta)
 
 	# safe_margin = 1.0 here helps to unstuck the ball from rotating platforms
 	# and possibly player controlled platform
 	var safe_margin := 1.0
-	var collision = ball.move_and_collide(ball_direction * ball_speed * delta,
+	var collision = ball.move_and_collide(_ball_velocity() * delta,
 		false, safe_margin)
 	if collision:
 		var normal: Vector2 = collision.get_normal()
+		var bounce_angle_limit := 0.0
 		if collision.get_collider() == player:
-			var offset = player.position.x - collision.get_position().x
+			var offset := player.position.x - collision.get_position().x
 			var shape: RectangleShape2D = player.get_child(0).shape
-			var offset_normalized = offset / shape.size.x
-			var angle_adjustment = offset_normalized * reflect_amount
-			var normal_offseted = Vector2.from_angle(normal.angle() - angle_adjustment)
+			var offset_normalized := offset / shape.size.x
+			var angle_adjustment := offset_normalized * reflect_amount
+			var normal_offseted := Vector2.from_angle(normal.angle() - angle_adjustment)
 			normal = normal_offseted
+			bounce_angle_limit = player_angle_limit
 		elif collision.get_collider() is Brick:
 			var brick := collision.get_collider() as Brick
 			if brick.apply_damage(101.0):
 				score += 1
+			bounce_angle_limit = brick_angle_limit
+		else:
+			bounce_angle_limit = wall_angle_limit
 
-		$Debug.bounce.velocity_in = ball_direction * ball_speed
+		$Debug.bounce.velocity_in = _ball_velocity()
 		ball_direction = ball_direction.bounce(normal).normalized()
-		$Debug.bounce.velocity_out = ball_direction * ball_speed
+		$Debug.bounce.velocity_out_unlimited = _ball_velocity()
+		ball_direction = _limit_bounce_angle(normal, ball_direction, bounce_angle_limit)
+		$Debug.bounce.velocity_out = _ball_velocity()
 		$Debug.bounce.position = collision.get_position()
 		$Debug.bounce.normal = normal
 		$Debug.queue_redraw()
@@ -80,7 +107,5 @@ func _process(_delta: float) -> void:
 	elif Input.is_action_pressed("MoveRight"):
 		direction = 1.0
 	player_velocity.x = direction * player_speed
-	player_velocity_avg = (player_velocity_avg + player_velocity) * 0.05 * _delta
-	$Debug.update_value("player_velocity_avg", player_velocity_avg)
 	$Debug.update_value("score", score)
 	$Debug.update_value("bricks.get_child_count()", bricks.get_child_count())
